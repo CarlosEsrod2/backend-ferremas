a626ae6add8e8eac9d0f0c1ae6b52053e6ff5ebe
CarlosEsrod2 <ca.espinozar@duocuc.cl>
2025-05-06 16:37:53 -0400

    API valor dolar diario
    se consume la API de mindicador.cl

---
+++ Creacion de tablas e insercion datos.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE ferremas;
+
+CREATE TABLE ferremas.product (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price INT(11) NOT NULL,
+    type INT(11) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ferremas.users (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO ferremas.product (name, price, type) VALUES
+
+-- Herramientas Manuales
+('Martillo Mango de Madera', 5990, 1),
+('Destornillador de Punta Plana 6mm', 3490, 1),
+('Llave Ajustable 8 pulgadas', 6990, 1),
+('Serrucho para Madera 20"', 5990, 1),
+
+-- Materiales Básicos
+('Clavos de Acero 1 pulgada (100u)', 2990, 2),
+('Plancha de MDF 18mm 122x244cm', 15990, 2),
+('Cemento Portland 25kg', 5890, 2),
+('Arena Fina 25kg', 2890, 2),
+
+-- Equipos de Seguridad
+('Casco de Seguridad Amarillo', 8990, 3),
+('Guantes de Cuero para Construcción', 4990, 3),
+('Lentes de Seguridad Transparentes', 1990, 3),
+('Botas de Seguridad con Punta de Acero T42', 25990, 3),
+
+-- Tornillos y Anclajes
+('Tornillos Philips 3x20mm (50u)', 1890, 4),
+('Tarugo Plástico 6mm (100u)', 1990, 4),
+('Anclajes de Expansión M10 (4u)', 3990, 4),
+('Tornillos para Madera 4x30mm (100u)', 2490, 4),
+
+-- Fijaciones y Adhesivos
+('Silicona Transparente 280ml', 3490, 5),
+('Pega Epóxica 2 Componentes 50ml', 5490, 5),
+('Cinta Doble Contacto 10m x 19mm', 2990, 5),
+('Adhesivo de Contacto 1L', 4790, 5),
+
+-- Equipos de Medición
+('Cinta Métrica 5m', 4990, 6),
+('Nivel de Burbuja 40cm', 6990, 6),
+('Medidor Láser de Distancia 40m', 24990, 6),
+('Escuadra Metálica 30cm', 3990, 6);
+
+
+
+
+
+
+
+
--- api/services/product_service.py
+++ api/services/product_service.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+import requests 
 from api.models.product import Product
 
 class ProductService:
@@ -8,14 +11,54 @@ class ProductService:
         cursor = self.mysql.connection.cursor()
         cursor.execute("SELECT id, name, price, type FROM product")
         results = cursor.fetchall()
-        products = [Product(id=row[0], name=row[1], price=row[2], type=row[3]).to_dict() for row in results]
-        return products
+        flag = False
+        allproducts = []
+        try:
+            # Obtener el valor del dólar desde la API
+            date = datetime.date.today()
+            dmy = date.strftime("%d-%m-%Y")
+            url = f'https://mindicador.cl/api/dolar/{dmy}'
+            response = requests.get(url)
+            data = json.loads(response.text.encode("utf-8"))
+            valor_dolar = data['serie'][0]['valor']
+            flag = True
+        except Exception as e:
+            #print(f"Error al obtener el valor del dólar: {e}")
+            valor_dolar = 'API - mindicador.cl - no disponible'
+
+        for row in results:
+            products = Product(id=row[0], name=row[1], price=row[2], type=row[3]).to_dict()
+            if flag:
+                products['valor_dolar'] = round(row[2]/valor_dolar,2)
+            else:
+                products['valor_dolar'] = valor_dolar
+            allproducts.append(products)
+        
+        #print(allproducts)
+        return allproducts
+        
     
+    #NO USADO
     def get_product_by_id(self, product_id):
         cursor = self.mysql.connection.cursor()
         cursor.execute("SELECT id, name, price, type FROM product WHERE id = %s", (product_id,))
-        row = cursor.fetchone()
-        if row:
+        results = cursor.fetchall()
+
+        for row in results:
+            # Obtener el valor del dólar desde la API
+            try:
+                date = datetime.date.today()
+                dmy = date.strftime("%d-%m-%Y")
+                url = f'https://mindicador.cl/api/dolar/{dmy}'
+                response = requests.get(url)
+                data = json.loads(response.text.encode("utf-8"))
+                valor_dolar = data['serie'][0]['valor']
+            except Exception as e:
+                print(f"Error al obtener el valor del dólar: {e}")
+                valor_dolar = None
             product = Product(id=row[0], name=row[1], price=row[2], type=row[3])
-            return product.to_dict()
+            dictproduct = product.to_dict()
+            dictproduct['valor_dolar'] = valor_dolar
+            print(f"Valor del dólar: {valor_dolar}")
+            return dictproduct
         return None
